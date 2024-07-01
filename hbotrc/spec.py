0d6fe05e8ab898c4441906f6afcd5ca205fa2cb7
Martin Kou <bitanarch@gmail.com>
2024-07-01 11:21:56 -0700

    (fix) Fixed missing message URIs

--- hbotrc/spec.py
+++ hbotrc/spec.py
@@ -9,6 +9,10 @@ class CommandTopicSpecs:
     BALANCE_LIMIT: str = '/balance/limit'
     BALANCE_PAPER: str = '/balance/paper'
     COMMAND_SHORTCUT: str = '/command_shortcuts'
+    EXCHANGE_INFO: str = '/exchange_info'
+    USER_DIRECTED_TRADE: str = '/user_directed/trade'
+    USER_DIRECTED_CANCEL: str = '/user_directed/cancel'
+    USER_DIRECTED_LIST_ACTIVE_ORDERS: str = '/user_directed/list_active_orders'
 
 
 class TopicSpecs:
